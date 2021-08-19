a5cb9fbd8c84b8b57fd146260e683577a9eb109b
Yevheniy Tkachenko <yevheniytkachenko@Mac-mini-Yevheniy.local>
2021-08-19 11:40:10 +0300

    unit tests for sqlc user

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,15 +2,17 @@ package db
 
 import (
 	"context"
+	"github.com/JekaTka/auth-service/util"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateUser(t *testing.T)  {
+func createRandomUser(t *testing.T) User {
 	arg := CreateUserParams{
-		Nickname: "test-user",
-		Email:    "email@test.com",
+		Nickname: util.RandomNickname(),
+		Email:    util.RandomEmail(),
 	}
 
 	user, err := testQueries.CreateUser(context.Background(), arg)
@@ -22,4 +24,22 @@ func TestCreateUser(t *testing.T)  {
 
 	require.NotZero(t, user.ID)
 	require.NotZero(t, user.CreatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T)  {
+	createRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	expectedUser := createRandomUser(t)
+	user, err := testQueries.GetUser(context.Background(), expectedUser.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+	require.Equal(t, expectedUser.ID, user.ID)
+	require.Equal(t, expectedUser.Email, user.Email)
+	require.Equal(t, expectedUser.Nickname, user.Nickname)
+	require.WithinDuration(t, expectedUser.CreatedAt.Time, user.CreatedAt.Time, time.Second)
 }
--- util/random.go
+++ util/random.go
@@ -1,1 +1,41 @@
 package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generates a random integer between min & max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of n characters
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomNickname generates a random nickname
+func RandomNickname() string {
+	return RandomString(6)
+}
+
+// RandomEmail generates a random email
+func RandomEmail() string {
+	return RandomString(6) + "@random.com"
+}
